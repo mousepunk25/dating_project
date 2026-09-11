36807e1c43975cf97b0953f157843e0f25a9c285
mousepunk <mousepunk25@gmail.com>
2026-09-11 16:04:15 -0400

    Adjust the CORS v2

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ const corsOptions = {
         }
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
