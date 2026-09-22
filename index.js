a7f139a250059f89ba2848294d473fc4ff986c1a
mousepunk <mousepunk25@gmail.com>
2026-09-22 15:27:43 -0400

    Change limits order

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
